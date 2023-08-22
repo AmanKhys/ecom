7dfd084b3e97e66d782ecd7a26c3170037d5980e
AmanKhys <amankhys2112@gmail.com>
2023-08-22 09:51:56 +0530

    make navbar and products container foe index.hbs parse fake db values with an array in index.js - stored product images in public/ - used bootstrap css, js

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let products = [
+    {
+      name: "iphone 11",
+      description: "this is an expensive phone",
+      image: "/images/vivo.jpeg"
+    },
+    {
+      name: "iphone 12",
+      description: "this is an expensive phone",
+      image: "/images/vivo.jpeg"
+    },
+    {
+      name: "iphone 13",
+      description: "this is an expensive phone",
+      image: "/images/vivo.jpeg"
+    },
+    {
+      name: "iphone 14",
+      description: "this is an expensive phone",
+      image: "/images/vivo.jpeg"
+    },
+  ]
+  res.render('index', { title: 'Express', products });
+});
+
+module.exports = router;
