d72001ebe2869445eb50f509eb7090476b79e713
AmanKhys <amankhys2112@gmail.com>
2023-08-25 11:18:07 +0530

    change to appropriate images for products

--- routes/index.js
+++ routes/index.js
@@ -7,22 +7,22 @@ router.get('/', function(req, res, next) {
     {
       name: "iphone 11",
       description: "this is an expensive phone",
-      image: "/images/vivo.jpeg"
+      image: "/images/iphone11.jpg"
     },
     {
       name: "iphone 12",
       description: "this is an expensive phone",
-      image: "/images/vivo.jpeg"
+      image: "/images/iphone12.jpg"
     },
     {
       name: "iphone 13",
       description: "this is an expensive phone",
-      image: "/images/vivo.jpeg"
+      image: "/images/iphone13.jpg"
     },
     {
       name: "iphone 14",
       description: "this is an expensive phone",
-      image: "/images/vivo.jpeg"
+      image: "/images/iphone14.jpg"
     },
   ]
   res.render('index', { title: 'Express', products, admin: false});
