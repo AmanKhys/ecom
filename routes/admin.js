7d7ad57acc1b3fafa95791883b149f207d4998aa
AmanKhys <amankhys2112@gmail.com>
2023-08-26 13:03:52 +0530

    make admin user have partial to view all products and add a form to add new products - setup routes for those get and post requests - change the layout to be included in a html tag and the scrpit and link   in the head tag

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+  let products = [
+    {
+      name: "iphone 11",
+      description: "this is an expensive phone",
+      category: "mobile",
+      image: "/images/iphone11.jpg"
+    },
+    {
+      name: "iphone 12",
+      description: "this is an expensive phone",
+      category: "mobile",
+      image: "/images/iphone12.jpg"
+    },
+    {
+      name: "iphone 13",
+      description: "this is an expensive phone",
+      category: "mobile",
+      image: "/images/iphone13.jpg"
+    },
+    {
+      name: "iphone 14",
+      description: "this is an expensive phone",
+      category: "mobile",
+      image: "/images/iphone14.jpg"
+    },
+  ]
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index')
+  res.render('admin/view-products', {admin: true, products})
 });
 
+router.get('/add-products', function(req, res, next) {
+  res.render('admin/add-products', {admin: true, products})
+});
+
+router.post('/admin/add-products', (res, req) => {
+  console.log(res.body)
+})
 module.exports = router;
